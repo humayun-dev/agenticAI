3eaa5399d5015ac8cde55d851b4c46e7ff02865a
humayun-dev <humayun.devv@gmail.com>
2025-11-11 11:32:34 +0500

    Chatbot with streamlit interface

---
+++ chatbot_streamlit_user_interface/chatbot_backend.py
@@ -0,0 +1,34 @@
+# Chatbot having user interface 
+# Author: Muhammad Humayun Khan
+
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+# load key and model
+load_dotenv()
+llm = ChatOpenAI()
+
+# create schema for the chatbot
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# create associated function for the node
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
